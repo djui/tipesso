a8e9e64abac1a74cbda440d56ad08c51a58953f2
Uwe Dauernheim <uwe@dauernheim.net>
2013-05-26 10:02:36 +0200

    Improve readability

--- src/tipesso/discoverer.clj
+++ src/tipesso/discoverer.clj
@@ -5,14 +5,11 @@
     (let  [buf (java.io.BufferedReader. (java.io.InputStreamReader. stream))]
       (apply str (line-seq buf)))))
 
-(defn seq->map [s]
-  (into {} (map vec (partition 2 s))))
-
 (defn discover [link]
   (let [[_ user repo] (re-matches #"^https://github.com/(.+?)/(.+?)$" link)
         project-url (format "https://raw.github.com/%s/%s/master/project.clj" user repo)
         project-content (fetch-url project-url)
         project-data (binding [*read-eval* false] (read-string project-content))
-        project-map (seq->map (drop 3 project-data))]
     (pr-str "'" (project-map :dependencies) "'")))
 
+        project-map     (apply hash-map (drop 3 project-data))]
