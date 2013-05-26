5f858f70ff5729c83e041e021ca1b9e3d0e34e6d
Uwe Dauernheim <uwe@dauernheim.net>
2013-05-26 22:10:37 +0200

    Serve index.html at root ("/"). Close #5.

--- src/tipesso/handler.clj
+++ src/tipesso/handler.clj
@@ -6,9 +6,11 @@
             [hiccup.core :only (html)]
             [hiccup.page :only (html5 include-js include-css)]
             [hiccup.element :only (link-to)]
-            [tipesso.discoverer :as discoverer]))
+            [tipesso.discoverer :as discoverer]
+            [ring.util.response :as response]))
 
 (defroutes app-routes
+  (GET "/" [] (response/resource-response "index.html" {:root "public"}))
   (GET "/project*" {{link "name"} :query-params} (discoverer/discover link))
   (route/resources "/")
   (route/not-found "Not Found"))
