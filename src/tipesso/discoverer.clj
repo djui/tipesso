694012828ba93b163c274f57ccc3f3a1cb2b8dd3
Uwe Dauernheim <uwe@dauernheim.net>
2013-05-26 10:02:52 +0200

    Clean up

--- src/tipesso/discoverer.clj
+++ src/tipesso/discoverer.clj
@@ -2,14 +2,13 @@
 
 (defn fetch-url [address]
   (with-open [stream (.openStream (java.net.URL. address))]
-    (let  [buf (java.io.BufferedReader. (java.io.InputStreamReader. stream))]
+    (let [buf (java.io.BufferedReader. (java.io.InputStreamReader. stream))]
       (apply str (line-seq buf)))))
 
 (defn discover [link]
-  (let [[_ user repo] (re-matches #"^https://github.com/(.+?)/(.+?)$" link)
-        project-url (format "https://raw.github.com/%s/%s/master/project.clj" user repo)
+  (let [[_ user repo]   (re-matches #"^https://github.com/(.+?)/(.+?)$" link)
+        project-url     (format "https://raw.github.com/%s/%s/master/project.clj" user repo)
         project-content (fetch-url project-url)
-        project-data (binding [*read-eval* false] (read-string project-content))
-    (pr-str "'" (project-map :dependencies) "'")))
-
+        project-data    (binding [*read-eval* false] (read-string project-content))
         project-map     (apply hash-map (drop 3 project-data))]
+    (pr-str (pr-str (project-map :dependencies)))))
