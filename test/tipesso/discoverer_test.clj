f815f571d452f1e8f0af3db89f388a634668107a
Uwe Dauernheim <uwe@dauernheim.net>
2013-06-06 11:06:53 +0200

    Fix rewrite 2.0

--- test/tipesso/discoverer_test.clj
+++ test/tipesso/discoverer_test.clj
@@ -3,27 +3,27 @@
         tipesso.discoverer))
 
 (deftest github-origin
-  (is (= :github (origin-type "http://github.com/djui/tipesso")))
-  (is (= :github (origin-type "https://github.com/djui/tipesso")))
-  (is (= :github (origin-type "http://github.com/djui/tipesso.git")))
-  (is (= :github (origin-type "https://github.com/djui/tipesso.git")))
-  (is (= :github (origin-type "http://github.com/djui/tipesso/master")))
-  (is (= :github (origin-type "https://github.com/djui/tipesso/master")))
-  (is (= :github (origin-type "http://djui.github.io/tipesso")))
-  (is (= :github (origin-type "https://djui.github.io/tipesso")))
-  (is (= :github (origin-type "http://djui.github.com/tipesso")))
-  (is (= :github (origin-type "https://djui.github.com/tipesso")))
-  (is (= :github (origin-type "http://djui.github.io/tipesso/index.html")))
-  (is (= :github (origin-type "https://djui.github.io/tipesso/index.html"))))
+  (is (github-project "http://github.com/djui/tipesso"))
+  (is (github-project "https://github.com/djui/tipesso"))
+  (is (github-project "http://github.com/djui/tipesso.git"))
+  (is (github-project "https://github.com/djui/tipesso.git"))
+  (is (github-project "http://github.com/djui/tipesso/master"))
+  (is (github-project "https://github.com/djui/tipesso/master"))
+  (is (github-project "http://djui.github.io/tipesso"))
+  (is (github-project "https://djui.github.io/tipesso"))
+  (is (github-project "http://djui.github.com/tipesso"))
+  (is (github-project "https://djui.github.com/tipesso"))
+  (is (github-project "http://djui.github.io/tipesso/index.html"))
+  (is (github-project "https://djui.github.io/tipesso/index.html")))
 
 (deftest unknown-origin
-  (is (= :UNKNOWN (origin-type "http://google.com"))))
+  (not (is (github-project "http://google.com"))))
 
-(deftest handle-github-origin
+#_(deftest handle-github-origin
   (is (= {:type :github
           :uri "https://github.com/djui/tipesso"
           :project {:name "tipesso"
                     :authors ["djui"]
                     ;; Make this pattern matching
                     :languages {:Clojure 1922 :JavaScript 256}}}
-         (handle-origin "https://github.com/djui/tipesso/"))))
+         (github-project "https://github.com/djui/tipesso/"))))
