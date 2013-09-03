96d839ad6ad9c91ab7648ac7fe7aa7a79c58fdac
Uwe Dauernheim <uwe@dauernheim.net>
2013-09-03 22:51:51 +0200

    Indicate Ajax progress and result

--- resources/public/js/discover.js
+++ resources/public/js/discover.js
@@ -1,5 +1,5 @@
 function DiscoverCtrl($scope, $http) {
-
+  
   // Initial recommendations
   $scope.tippables = [
       {"location": "github",
@@ -9,13 +9,31 @@ function DiscoverCtrl($scope, $http) {
        "username": "onlyafly",
        "reason": "Coauthor of Tipesso!"}
   ];
-
+  
+  $scope.$on('discovery.begin', function() {
+    $scope.discovery_loading = true;
+    $scope.discovery_error = false;
+  });
+  $scope.$on('discovery.success', function() {
+    $scope.discovery_loading = false;
+    $scope.discovery_error = false;
+  });
+  $scope.$on('discovery.error', function() {
+    $scope.discovery_loading = false;
+    $scope.discovery_error = true;
+  });
+  
   $scope.discover = function() {
-    $http({url: '/project',
-           method: 'GET',
-           params: {'name': $scope.tipProject}
-          }).success(function(data) {
-              $scope.tippables = data;
-          });
-  };
+    var successCB = function(data) {
+      $scope.$emit('discovery.success');
+      $scope.tippables = data;
+    };
+    var errorCB = function(data, code) {
+      $scope.$emit('discovery.error');
+    };
+    
+    $scope.$emit('discovery.begin');
+    var params = {params: {'name': $scope.tipProject}};
+    $http.get('/project', params).success(successCB).error(errorCB);
+};
 }
